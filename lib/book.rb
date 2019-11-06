f6f2241a3e4d7e6e8ad773e8da716516a56e2f1e
Logan Smith <logsmith06@gmail.com>
2019-11-06 21:12:29 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
 
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = page_count
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+
+  end
+
+  def page_count
+    x = 272
+    return x
+  end
+end
+
+Book.new("And Then THere Were None")
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
 # Make your shoe class here!
+
+
+class Shoe
+  new_condition = ""
+  def initialize(brand = "Nike")
+    @brand = brand
+  end
+tester = 0
+  attr_accessor :brand, :color, :size, :material, :condition
+
+
+
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+
+
+
+
+
+
+end
